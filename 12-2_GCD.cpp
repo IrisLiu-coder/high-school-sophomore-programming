b1bb88ffb03bdd6db7741bced35463a7edc35ee8
劉邦均 <83829881+IrisLiu-coder@users.noreply.github.com>
2022-01-10 01:30:20 +0800

    Create 12-2_GCD.cpp

---
+++ 12-2_GCD.cpp
@@ -0,0 +1,16 @@
+/*用遞迴解寫出 2正整數的 GCD*/
+#include <iostream>
+using namespace std;
+int gcd(int a,int b){
+    if (b==0){
+        return a;
+    }
+    else{
+        return gcd(b,a%b);
+    }
+}
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<gcd(a,b);
+}
