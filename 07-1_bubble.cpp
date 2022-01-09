8ff9a035005939fd3bd70bedb439ed61afe23181
劉邦均 <83829881+IrisLiu-coder@users.noreply.github.com>
2022-01-10 01:07:56 +0800

    Create 07-1_bubble.cpp

---
+++ 07-1_bubble.cpp
@@ -0,0 +1,28 @@
+/*列出氣泡排序法的運算過程，若已排序完成即停止比較*/
+#include <iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+    bool f;
+    cin>>n;
+    int a[n]={};
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(i=0;i<n;i++){
+        f=0;
+        for(j=0;j<n-i;j++){
+            if(a[j]>a[j+1]){
+                swap(a[j],a[j+1]);
+                f=1;
+            }
+        }
+        if(f==0){
+            break;
+        }
+        for(j=0;j<n;j++){
+            cout<<a[j]<<" ";
+        }
+        cout<<"\n";
+    }
+}
