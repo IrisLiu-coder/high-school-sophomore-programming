f1b567283af0d5cccaf0ca78b3efe58069ff52b6
劉邦均 <83829881+IrisLiu-coder@users.noreply.github.com>
2022-01-10 00:40:25 +0800

    Create 03-2_while_practice.cpp

---
+++ 03-2_while_practice.cpp
@@ -0,0 +1,12 @@
+/*輸入一數列，計算第一個0出現前的數字個數 
+(使用while指令，不使用if)*/
+#include <iostream>
+using namespace std;
+int main(){
+    int i=0,n;
+    while(n!=0){
+        i=i+1;
+        cin>>n;
+    }
+    cout<<i-1;
+}
