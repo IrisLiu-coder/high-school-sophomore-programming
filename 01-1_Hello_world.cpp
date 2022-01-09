0b66f19c0a6dc09f931aa0f8f1f4f501384ecd74
劉邦均 <83829881+IrisLiu-coder@users.noreply.github.com>
2022-01-10 00:28:20 +0800

    Rename 01_Hello_world.cpp to 01-1_Hello_world.cpp

---
+++ 01-1_Hello_world.cpp
@@ -0,0 +1,9 @@
+/*輸入名字，輸出 Hello [人名]。*/
+#include <iostream>
+using namespace std;
+int main(){
+string a;
+cout<<"請輸入姓名\n";
+cin>>a;
+cout<<"Hello "<<a<<endl;
+}
--- 01_Hello_world.cpp
+++
@@ -1,9 +0,0 @@
-/*輸入名字，輸出 Hello [人名]。*/
-#include <iostream>
-using namespace std;
-int main(){
-string a;
-cout<<"請輸入姓名\n";
-cin>>a;
-cout<<"Hello "<<a<<endl;
-}
